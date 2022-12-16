d5acc051187896676bbf96207f05c35a768b4cce
Nguyen Loi <nguyenloi2908@icloud.com>
2022-12-16 11:41:57 +0700

    Create second View

---
+++ learn-git-github-inxcode/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  learn-git-github-inxcode
+//
+//  Created by Loi Nguyen on 16/12/2022.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
