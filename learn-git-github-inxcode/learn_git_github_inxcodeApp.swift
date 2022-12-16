10d895893c0bd32cdfbac1148f4468679b6af4ac
Nguyen Loi <nguyenloi2908@icloud.com>
2022-12-16 11:19:41 +0700

    Initial Commit

---
+++ learn-git-github-inxcode/learn_git_github_inxcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_git_github_inxcodeApp.swift
+//  learn-git-github-inxcode
+//
+//  Created by Loi Nguyen on 16/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct learn_git_github_inxcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
